70afc72099c343c155d6aee024731265d8a62f6c
matheusteixeira <matheusteixeira01@live.com>
2019-05-12 20:40:45 -0300

    Fix briot_ruffini

--- t2/briot_ruffini.m
+++ t2/briot_ruffini.m
@@ -1,10 +1,14 @@
-function [b remainder] = briot_ruffini(a, xi)
+function [b R] = briot_ruffini(a, xi)
+  if(length(a) == 1)
+    b = [0];
+    R = a(1);
+  else
+    b(1) = a(1);
 
-  b(1) = a(1);
+    for i = 2 : length(a) - 1
+        b(i) = a(i) + xi*b(i-1);
+    end
 
-  for i = 2 : length(a) - 1
-      b(i) = a(i) + xi*b(i-1);
+    R = a(end) + xi*b(end);
   end
-
-  remainder = a(end) + xi*b(end);
 end
--- t2/main.m
+++ t2/main.m
@@ -1,4 +1,4 @@
-format long
+% format long
 %  1). Determine os zeros REAIS da função f(x) = x*tan(x)-1, e seus resíduos,
 % entre -2*pi e 2*pi. Lembre-se de verificar e evitar pontos de descontinuidades
 % (nesse caso existem descontinuidades em pi/2 e seus múltiplos);
@@ -17,16 +17,18 @@ residuo = f(newton_roots)
 a = [ +1 -7  +20.95 -34.75 +34.5004 -20.5012 +6.7512 -0.9504 ];
 n = length(a) - 1;
 
-xi = locate_polynomial_roots(a, n, 0.1);
+xi = locate_polynomial_roots(a, n, 0.001);
 
 % 2a). Use o método de Newton tradicional (com multiplicidade M=1);
 traditional_newton_roots = traditional_newton(xi, a, n, tolerance)
 
-residuo = abs(solve_horner(a, traditional_newton_roots))
+residuo = abs(solve_horner(a, traditional_newton_roots));
 
 % 2b). Use o método de Newton, usando a estimativa da multiplicidade M de
 % cada raiz (teste diferentes valores de raízes iniciais e diferentes limites (1e-2, 1e-3,...)
 % para a soma dos restos na estimativa da multiplicidade).
+traditional_newton_roots_with_multiplicity = traditional_newton_with_multiplicity(xi, a, n, tolerance, 1e-6)
+
 
 % 2c). Monte o polinômio fatorado em binômios (x-raiz(1))^M(1) * (x-raiz(2))^M(2) * ....., com as raizes arredondadas paar o seu valor exato.
 
@@ -38,9 +40,19 @@ residuo = abs(solve_horner(a, traditional_newton_roots))
 % x = + 1.100000000070061 + 0.000000000000000 i com M = 1
 % x = + 1.200000000046985 + 0.000000000000000 i com M = 1;
 
-% ARREDONDE PARA GERAR O POLINÔMIO EXATO.
+printf('Polinomio fatorado em binômios:\n')
+@(x) (x - 0.8)^1 * (x - 0.9)^1 * (x - 1)^3 * (x - 1.1)^1 * (x - 1.2)^1
 
+% ARREDONDE PARA GERAR O POLINÔMIO EXATO.
+printf('\n')
 % 2d). Obtenha e imprime as raízes pela função  roots do Octave e pelo WolfraAlfa.
+octave_roots = roots(a)
+
+printf("wolframalpha_roots =\n");
+printf("0.8\n0.9\n1.00019\n1.1\n1.2\n");
+printf("0.999907 - 0.000160622 i\n");
+printf("0.999907 + 0.000160622 i\n");
+printf('\n')
 
 % 3). Dado o sistema:
 
@@ -55,4 +67,13 @@ residuo = abs(solve_horner(a, traditional_newton_roots))
 % - o resíduo máximo das equações do sistema de  equações não lineares
 
 % Escolha valores iniciais (radianos) e critério de parada compatível com a variavel double.
+
+xi = [-0.5; 0.5];
+
+y1 = @(x) x(1)^3 + x(2)^2 - 2;
+y2 = @(x) sin(x(1))*cos(x(2)) - 0.45;
+
+newton_with_derivatives = sysnewton(xi, y1, y2, tolerance)
+
+residuo = max(abs([y1(newton_with_derivatives) y2(newton_with_derivatives)]))
 
